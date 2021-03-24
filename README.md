51838ddbdc7096d2ce36615eb6d49deebdc918e0
Skylar Kwon <ddauppo@gmail.com>
2021-03-24 15:37:12 -0400

    Create Customer.js

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 ## Management Systema
 
 This project is customer management System with React.js.
-
-# Getting Started with Create React App
-
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers =[
+   {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': 'Elisa Ng',
+  'birthday': '210211',
+  'gender': 'Fmail',
+  'job': 'Students'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': 'James Tommer',
+  'birthday': '210221',
+  'gender': 'Male',
+  'job': 'Programmer'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': 'Skylar Kwon',
+  'birthday': '210321',
+  'gender': 'Femail',
+  'job': 'Doctor'
+}
+]
+
 
 function App() {
   return (
-    <div className="gray-background">
-        <img src={logo} alt="logo" />
-        <h2> Students Management System.</h2>
+    <div>
+   {
+     customers.map( c => {
+       return (
+         <Customer
+         key= {c.id}
+         id={ c.id }
+         image={ c.image}
+         name={ c.name}
+         birthday={ c.birthday }
+         gender={ c.gender}
+         job ={ c.job }
+         />  
+       )
+     })
+   }
     </div>
   );
 }

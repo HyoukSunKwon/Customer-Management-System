2a3a083e60ce1035124cfba432fece4ee47bf835
Skylar Kwon <ddauppo@gmail.com>
2021-03-31 22:01:35 -0400

    create customer database table & Node.js database connection setting

--- server.js
+++ server.js
@@ -3,36 +3,38 @@ const bodyParser = require('body-parser')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const mysql = require('mysql');
+
+function createConnection() {
+  return mysql.createConnection({
+     host: "127.0.0.1",
+     user: "root",
+     password: "0426",
+     database: "management",
+     port: 3306,
+  });
+}
+
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/api/customers' , (req, res) =>{
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': 'Elisa Ng',
-            'birthday': '210211',
-            'gender': 'Fmail',
-            'job': 'Students'
-          },
-          {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': 'James Tommer',
-            'birthday': '210221',
-            'gender': 'Male',
-            'job': 'Programmer'
-          },
-          {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': 'Skylar Kwon',
-            'birthday': '210321',
-            'gender': 'Femail',
-            'job': 'Doctor'
-          }
-    ])
-});
+  let connection;
+  connection = createConnection( );
+  connection.connect();
+  let query = 'Select * from customer';
+  connection.query(query, function(error, result){
+    if(error) {
+      console.log(error);
+    }else {
+      res.send(result);
+    }
+  });
+  console.log("here");
+})
+
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

ef633724242e75f78ff3ac9f2857a613e300f391
Skylar Kwon <ddauppo@gmail.com>
2021-04-06 15:50:07 -0400

    Connection with server

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ import { render } from '@testing-library/react';
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX: "auto"
   },
   table: {
@@ -26,7 +26,7 @@ const styles = theme => ({
     color: theme.palette.common.white,
   },
   progress: {
-    margin: theme.spacing.unit * 2
+    margin: theme.spacing(2)
   }
 })
 
--- server.js
+++ server.js
@@ -2,37 +2,41 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 const port = process.env.PORT || 5000;
+const fs = require('fs');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/customers' , (req, res) =>{
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': 'Elisa Ng',
-            'birthday': '210211',
-            'gender': 'Fmail',
-            'job': 'Students'
-          },
-          {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': 'James Tommer',
-            'birthday': '210221',
-            'gender': 'Male',
-            'job': 'Programmer'
-          },
-          {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': 'Skylar Kwon',
-            'birthday': '210321',
-            'gender': 'Femail',
-            'job': 'Doctor'
-          }
-    ])
+const data =fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql =  require('mysql');
+const { userInfo } = require('os');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
 });
+
+connection.connect();
+
+app.get('/api/customers' , (req, res) =>{
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err, rows) => {
+      if(err) {
+        console.log("cannot get the results");
+      } else {
+        res.send(rows);
+      }     
+    }
+  );
+});
+
+
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

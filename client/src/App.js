58da56545cda3aae66fc31052f2be54557a606ae
Skylar Kwon <ddauppo@gmail.com>
2021-04-14 17:45:50 -0400

    Apply AppBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,105 +1,218 @@
-import React, { Component } from 'react';
-import './App.css';
-import Customer from './components/Customer';
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-import {  withStyles, makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { render } from '@testing-library/react';
-import CustomerAdd from './components/CustomerAdd';
+import React, { Component } from "react";
+import "./App.css";
+import Customer from "./components/Customer";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { render } from "@testing-library/react";
+import CustomerAdd from "./components/CustomerAdd";
 
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import { fade } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
-    width: '100%',
-    marginTop: theme.spacing(3),
-    overflowX: "auto"
+    width: "100%",
+    minWidth: 1080,
   },
-  table: {
-    minWidth: 1080
+  tableHead: {
+    fontSize: "1.0rem",
+    fontWeight: "bold",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
   },
   head: {
     backgroundColor: theme.palette.common.black,
     color: theme.palette.common.white,
   },
   progress: {
-    margin: theme.spacing(2)
-  }
-})
+    margin: theme.spacing(2),
+  },
+});
 
-class App extends Component{
-constructor(props){
-  super(props);
-  this.state = {
-    customers: '',
-    completed: 0
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: "",
+      completed: 0,
+    };
   }
-}
 
   stateRefresh = () => {
     this.setState({
-      customers: '',
-      completed: 0
+      customers: "",
+      completed: 0,
     });
     this.callApi()
-    .then(res => this.setState({ customers: res}))
-    .catch(err => console.log(err));
-  }
+      .then((res) => this.setState({ customers: res }))
+      .catch((err) => console.log(err));
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     this.timer = setInterval(this.progress, 20);
     this.callApi()
-    .then(res => this.setState({ customers: res}))
-    .catch(err => console.log(err));
+      .then((res) => this.setState({ customers: res }))
+      .catch((err) => console.log(err));
   }
 
   callApi = async () => {
-    const response = await fetch('/api/customers');
+    const response = await fetch("/api/customers");
     const body = await response.json();
     return body;
-  }
+  };
 
   progress = () => {
     const { completed } = this.state;
-    this.setState({ completed: completed >= 100 ? 0: completed + 1})
-  }
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
+  };
 
-  render(){
-  const { classes } =this.props;
-  return (
-    <div>
-        <Paper className={ classes.root}>     
-        <Table className={ classes.table }>
-          <TableHead >
-            <TableRow >
-              <TableCell className= { classes.head } > Id </TableCell>
-              <TableCell className= { classes.head }> Image </TableCell>
-              <TableCell className= { classes.head }> Name </TableCell>
-              <TableCell className= { classes.head }> Birthday </TableCell>
-              <TableCell className= { classes.head }> Gender </TableCell>
-              <TableCell className= { classes.head }> Job </TableCell>
-              <TableCell className= { classes.head }> Setting </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-          { this.state.customers ? this.state.customers.map( c => { return (
-          <Customer stateRefresh={this.stateRefresh} key= { c.id } id={ c.id } image={ c.image } name={ c.name } birthday={ c.birthday } gender={ c.gender} job ={ c.job } /> )})
-            : 
-          <TableRow>
-            <TableCell colSpan="6" align="center">
-              <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
-            </TableCell>
-          </TableRow>}
-          </TableBody>
-        </Table>
-      </Paper>
-      <CustomerAdd  stateRefresh={ this.stateRefresh } />
-    </div>
-  );}
+  render() {
+    const { classes } = this.props;
+    const cellList = [
+      "ID",
+      "Image",
+      "Name",
+      "Birthday",
+      "Gender",
+      "Job",
+      "Setting",
+    ];
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="open drawer"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" noWrap>
+              Customer Management System
+            </Typography>
+            <div>
+              <CustomerAdd stateRefresh={this.stateRefresh} />
+            </div>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ "aria-label": "search" }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Paper>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                {cellList.map((c) => {
+                  return (
+                    <TableCell className={classes.tableHead}> {c} </TableCell>
+                  );
+                })}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.customers ? (
+                this.state.customers.map((c) => {
+                  return (
+                    <Customer
+                      stateRefresh={this.stateRefresh}
+                      key={c.id}
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                    />
+                  );
+                })
+              ) : (
+                <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress
+                      className={classes.progress}
+                      variant="determinate"
+                      value={this.state.completed}
+                    />
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </Paper>
+      </div>
+    );
+  }
 }
 
-export default withStyles(styles) (App);
+export default withStyles(styles)(App);
--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -1,139 +1,201 @@
-import React from 'react';
-import { post } from 'axios';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { withStyles } from '@material-ui/core/styles';
+import React from "react";
+import { post } from "axios";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
 
-const styles = theme => ({
-    hidden: {
-        display: 'none'
-    }
-})
-
+const styles = (theme) => ({
+  hidden: {
+    display: "none",
+  },
+  add: {
+    height: 33,
+    alignItem: "center",
+    justifyContent: "center",
+    backgroundColor: "pailBlue",
+    fontWeight: "bold",
+  },
+});
 
 class CustomerAdd extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: null,
+      userName: "",
+      birthday: "",
+      gender: "",
+      job: "",
+      fileName: "",
+      open: false,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            file: null,
-            userName: '',
-            birthday: '',
-            gender:'',
-            job:'',
-            fileName:'',
-            open: false
-        }
-    }
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    this.addCustomer().then((res) => {
+      console.log(res.data);
+      this.props.stateRefresh();
+    });
+    this.setState({
+      file: null,
+      userName: "",
+      birthday: "",
+      gender: "",
+      job: "",
+      fileName: "",
+      open: false,
+    });
+  };
 
-    handleFormSubmit = (e) =>{
-        e.preventDefault()
-        this.addCustomer()
-        .then((res) => {
-            console.log(res.data);
-            this.props.stateRefresh();
-        })
-        this.setState({
-            file: null,
-            userName:'',
-            birthday:'',
-            gender:'',
-            job: '',
-            fileName: '',
-            open: false
-        })
-    }
+  handleFilechange = (e) => {
+    this.setState({
+      file: e.target.files[0],
+      fileName: e.target.value,
+    });
+  };
 
-    handleFilechange=(e) => {
-        this.setState({
-            file: e.target.files[0],
-            fileName: e.target.value
-        })
-    }
+  handleValueChange = (e) => {
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  };
 
-    handleValueChange =(e)=>{
-        let nextState = {};
-        nextState[e.target.name] = e.target.value;
-        this.setState(nextState);
-    }
+  addCustomer = () => {
+    const url = "/api/customers";
+    const formData = new FormData();
+    formData.append("image", this.state.file);
+    formData.append("name", this.state.userName);
+    formData.append("birthday", this.state.birthday);
+    formData.append("gender", this.state.gender);
+    formData.append("job", this.state.job);
+    const config = {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    };
 
-    addCustomer = () => {
-        const url = '/api/customers';
-        const formData = new FormData();
-        formData.append('image', this.state.file);
-        formData.append('name', this.state.userName);
-        formData.append('birthday', this.state.birthday);
-        formData.append('gender', this.state.gender);
-        formData.append('job', this.state.job);
-        const config = {
-            headers: {
-            'content-type': 'multipart/form-data'
-            }
-        }
+    return post(url, formData, config);
+  };
 
-        return post(url, formData, config);
-    }
+  handleOpen = () => {
+    this.setState({
+      open: true,
+    });
+  };
 
-    handleOpen = () => {
-        this.setState({
-            open: true
-        })
-    }
+  handleClose = () => {
+    this.setState({
+      file: null,
+      userName: "",
+      birthday: "",
+      gender: "",
+      job: "",
+      fileName: "",
+      open: false,
+    });
+  };
 
-    handleClose = () =>{
-        this.setState({
-            file:null,
-            userName:'',
-            birthday: '',
-            gender: '',
-            job: '',
-            fileName: '',
-            open: false
-        })
-    }
-    
-    render(){
-        const { classes } = this.props;
-        return(
-            <div>
-                <button variant="contained" color="primary" onClick={this.handleOpen}>
-                    Add Customers
-                </button>
-                <Dialog open={this.state.open}>
-                    <DialogTitle> Add Customer</DialogTitle>
-                    <DialogContent>
-                    <input  className={classes.hidden} accept="image/*"  id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFilechange} />
-                    <label htmlFor="raised-button-file">
-                        <Button variant="contained" color="primary" component="span" name="file">
-                            {this.state.fileName === "" ? " Select Profile Image " : this.state.fileName }
-                        </Button>
-                    </label> <br />
-                    <TextField label="Name" type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /> <br />
-                    <TextField label="Birthday" type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange} /> <br />
-                    <TextField label="Gender" type="text"  name="gender" value={this.state.gender} onChange={this.handleValueChange} /> <br />
-                    <TextField label="Job" type="text"  name="job" value={this.state.job} onChange={this.handleValueChange} /> <br />
-                    </DialogContent>
-                    <DialogActions>
-                        <Button variant="contained" clolor="primary" onClick={this.handleFormSubmit}> Add </Button>
-                        <Button variant="outlined" clolor="primary" onClick={this.handleClose}> Close </Button>
-                    </DialogActions>
-                </Dialog>
-            </div>
-            // <form onSubmit={this.handleFormSubmit}>
-            //     <h1>Add Customer</h1>
-            //     Image: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFilechange} /> <br/>
-            //     Name: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /> <br />
-            //     Birthday: <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange} /> <br />
-            //     Gender: <input type="text"  name="gender" value={this.state.gender} onChange={this.handleValueChange} /> <br />
-            //     Job: <input type="text"  name="job" value={this.state.job} onChange={this.handleValueChange} /> <br />
-            //     <button type="submit"> Add Customer </button>
-            // </form>
-        )
-    }
-} 
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <button className={classes.add} onClick={this.handleOpen}>
+          Add Customers
+        </button>
+        <Dialog open={this.state.open}>
+          <DialogTitle> Add Customer</DialogTitle>
+          <DialogContent>
+            <input
+              className={classes.hidden}
+              accept="image/*"
+              id="raised-button-file"
+              type="file"
+              file={this.state.file}
+              value={this.state.fileName}
+              onChange={this.handleFilechange}
+            />
+            <label htmlFor="raised-button-file">
+              <Button
+                variant="contained"
+                color="primary"
+                component="span"
+                name="file"
+              >
+                {this.state.fileName === ""
+                  ? " Select Profile Image "
+                  : this.state.fileName}
+              </Button>
+            </label>{" "}
+            <br />
+            <TextField
+              label="Name"
+              type="text"
+              name="userName"
+              value={this.state.userName}
+              onChange={this.handleValueChange}
+            />{" "}
+            <br />
+            <TextField
+              label="Birthday"
+              type="text"
+              name="birthday"
+              value={this.state.birthday}
+              onChange={this.handleValueChange}
+            />{" "}
+            <br />
+            <TextField
+              label="Gender"
+              type="text"
+              name="gender"
+              value={this.state.gender}
+              onChange={this.handleValueChange}
+            />{" "}
+            <br />
+            <TextField
+              label="Job"
+              type="text"
+              name="job"
+              value={this.state.job}
+              onChange={this.handleValueChange}
+            />{" "}
+            <br />
+          </DialogContent>
+          <DialogActions>
+            <Button
+              variant="contained"
+              clolor="primary"
+              onClick={this.handleFormSubmit}
+            >
+              {" "}
+              Add{" "}
+            </Button>
+            <Button
+              variant="outlined"
+              clolor="primary"
+              onClick={this.handleClose}
+            >
+              {" "}
+              Close{" "}
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+      // <form onSubmit={this.handleFormSubmit}>
+      //     <h1>Add Customer</h1>
+      //     Image: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFilechange} /> <br/>
+      //     Name: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /> <br />
+      //     Birthday: <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange} /> <br />
+      //     Gender: <input type="text"  name="gender" value={this.state.gender} onChange={this.handleValueChange} /> <br />
+      //     Job: <input type="text"  name="job" value={this.state.job} onChange={this.handleValueChange} /> <br />
+      //     <button type="submit"> Add Customer </button>
+      // </form>
+    );
+  }
+}
 
 export default withStyles(styles)(CustomerAdd);
